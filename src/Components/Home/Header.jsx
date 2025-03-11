c7cf43d5a2d2f92692d7f666841d7e2349e7f117
iamshreyashjain <shreyashjain41@gmail.com>
2025-03-11 14:58:43 +0530

    SCRUM-1-Creating-a-sidebar-with-reactIcons initiated+

--- src/Components/Home/Header.jsx
+++ src/Components/Home/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
         <>
-            <div className="bg-amber-600 w-1/2">Header</div>
+            <div className="bg-pink-600 w-1/2">Header</div>
         </>
     )
 }
